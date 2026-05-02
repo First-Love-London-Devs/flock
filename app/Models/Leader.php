b2e04591ff7114841036e2d8196f4bb13d20b749
Adebimpe Godwin <adebimpegodwin@MacBook-Pro.local>
2026-05-02 11:17:08 +0100

    feat: CheckRole middleware uses LeaderRole instead of user_type

--- app/Models/Leader.php
+++ app/Models/Leader.php
@@ -3,16 +3,16 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Support\Facades\Hash;
 use Laravel\Sanctum\HasApiTokens;
 use Spatie\Activitylog\LogOptions;
 use Spatie\Activitylog\Traits\LogsActivity;
 
-class Leader extends Model
+class Leader extends Authenticatable
 {
     use HasFactory, HasApiTokens, LogsActivity;
 
