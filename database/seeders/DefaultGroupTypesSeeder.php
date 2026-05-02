1b75d91d7a19d183c5a8cb20651660a840503596
Adebimpe Godwin <adebimpegodwin@MacBook-Pro.local>
2026-05-02 11:08:27 +0100

    feat: seed Constituency type and Bishop/Governor roles

--- database/seeders/DefaultGroupTypesSeeder.php
+++ database/seeders/DefaultGroupTypesSeeder.php
@@ -13,6 +13,7 @@ class DefaultGroupTypesSeeder extends Seeder
             ['name' => 'Zone', 'slug' => 'zone', 'level' => 0, 'tracks_attendance' => false, 'icon' => 'heroicon-o-globe-alt', 'color' => '#6366f1'],
             ['name' => 'District', 'slug' => 'district', 'level' => 1, 'tracks_attendance' => false, 'icon' => 'heroicon-o-building-office', 'color' => '#8b5cf6'],
             ['name' => 'Cell Group', 'slug' => 'cell-group', 'level' => 2, 'tracks_attendance' => true, 'icon' => 'heroicon-o-user-group', 'color' => '#10b981'],
+            ['name' => 'Constituency', 'slug' => 'constituency', 'level' => 0, 'tracks_attendance' => false, 'icon' => 'heroicon-o-flag', 'color' => '#0ea5e9'],
         ];
 
         foreach ($types as $type) {
